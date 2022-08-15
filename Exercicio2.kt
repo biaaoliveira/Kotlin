3c526232f07ee3c97368fb24cf05eec90a4a03f6
Bianca Oliveira <101846059+biaaoliveira@users.noreply.github.com>
2022-08-15 11:01:44 -0300

    Update Exercicio2.kt

---
+++ Exercicio2.kt
@@ -0,0 +1,10 @@
+fun main (){
+    print("Digite o diâmetro do círculo: ")
+    val diam = readln().toFloat()
+
+    val raio: Float = diam/2
+    val area = 3.14 * (raio * raio)
+    val peri = 2 * 3.14 * raio
+
+    print ("A area é de: " + area + "e o perimetro é: " + perimetro)
+}
